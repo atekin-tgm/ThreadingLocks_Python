8e41c16e3f14fd972f8e0934c97347849ff2ede0
atekin-tgm <atekin@student.tgm.ac.at>
2016-11-13 17:18:28 +0100

    add changes to Sum.py

--- Sum.py
+++ Sum.py
@@ -7,32 +7,70 @@
 # importing threading to be able to use Threads
 import threading
 
+num = int(input("Geben Sie bitte eine Zahl ein, bis zu der addiert werden soll!"))
+num_thread = input("Geben Sie die Anzahl der Threads ein!")
 
 class SumThread(threading.Thread):
     """
     class for addition
     """
 
-    # num_thread = input("Geben Sie die Anzahl der Threads ein!")
-    num = int(input("Geben Sie bitte eine Zahl ein, bis zu der addiert werden soll!"))
+
+
 
     lock = threading.Lock()
 
     counter = 0
 
-    def __init__(self):
+    def __init__(self, num):
         """
         init method
         """
         threading.Thread.__init__(self)
 
-    def run(self):
-        for i in range(self.num):
-            with SumThread.lock:
+        num = num
 
-                curValue = SumThread.counter
-                SumThread.counter = curValue
+
+    def run(self):
+        counter = 0
+
+        for i in range(self.num):
+            counter += i
+
+        with SumThread.lock:
+            SumThread.counter += counter
+
+        print (SumThread.counter)
 
 
 class Sum:
-    pass
+    def __init__(self, number, thread_num):
+        """
+        Splits the number into the amount of threads -> 3 Threads
+        :param number:
+        :param thread_num:
+        """
+
+        number = number
+        thread_num = 3
+
+        threads = []
+
+        split = number / thread_num
+
+        with SumThread.lock:
+            SumThread.total = 0
+
+        for i in range(thread_num):
+            start = split * i
+            end = start + split
+
+            thread = SumThread(round(start), round(end))
+            threads.append(thread)
+            thread.start()
+
+        for thread in threads:
+            thread.join()
+
+        end_num = SumThread.total
+        print (end_num)
