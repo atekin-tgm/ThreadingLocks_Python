9f61e17b64edaa8aece5d9c2e50ed29fe023fd83
atekin-tgm <atekin@student.tgm.ac.at>
2016-11-13 19:07:36 +0100

    finish thread synchronisation in python - SumThreading_Python

--- Sum.py
+++ Sum.py
@@ -7,12 +7,13 @@
 # importing threading to be able to use Threads
 import threading
 
-num = int(input("Geben sie eine Zahl ein!"))
-num_thread = 3
+# inputs for number and amount of threads
+num = int(input("What is your chosen number?"))
+num_thread = int(input("How many Threads do you want?"))
 
 class SumThread(threading.Thread):
     """
-    class for addition
+    class for counting with the run-method
     """
     lock = threading.Lock()
     counter = 0
@@ -25,9 +26,10 @@ class SumThread(threading.Thread):
         self.num = num
         self.thread_num = thread_num
 
+    # run-method for counting up
     def run(self):
         counter2 = 0
-        for i in range(int(self.num)):
+        for i in range(int(self.num+1), self.thread_num+1):
             counter2 += i
 
         with SumThread.lock:
@@ -37,22 +39,32 @@ class SumThread(threading.Thread):
 class Sum:
     def __init__(self):
         """
-        Splits the number into the amount of threads -> 3 Threads
+        Splits the number into the amount of threads and gives everything needed to SumThread
         :param number:
         :param thread_num:
         """
 
         threads = []
+
+        # number gets splittet by amount of threads
         split = num / num_thread
 
+        # start and end is defined -> SumThread
         for i in range(num_thread):
-            thread = SumThread(split, i)
+            start = i * split
+            end = start + split
+
+            thread = SumThread(round(start), round(end))
             threads += [thread]
             thread.start()
 
         for thread in threads:
             thread.join()
 
-        print (SumThread.counter)
+        # printing the result
+        print ("=================================")
+        print ("Number: " + str(num))
+        print ("Threads: " + str(num_thread))
+        print ("Result: " + str(SumThread.counter))
 
 Sum()
